c4dcf3665d5faff52a1b2ce6cc872765778f176a
BrycenAsay <brycen1177@gmail.com>
2023-02-09 11:47:23 -0700

    added headers, payload, and formated an API_CALLS class which stores the API calls and paramaters for the API calls

--- API_Calls.py
+++ API_Calls.py
@@ -1,12 +1,17 @@
 # creating a dictionary to store all twitter APIs and respective headers
 from config import BEARER_TOKEN as bear_token
-from support_funcs import get_user_id
+
+# headers for all API Calls
 HEADERS = {
     'Authorization': 'Bearer ' + bear_token,
     'Cookie': 'guest_id=v1%3A167354671801418520; guest_id_ads=v1%3A167354671801418520; guest_id_marketing=v1%3A167354671801418520; personalization_id="v1_Yg76iBwLIOAcAZZ145rAAA=="'
     }
+
+# payload for all API Calls
 PAYLOAD = {}
-class API_Calls:
+
+# create a class called API_Calls which stores all urls and proper peramaters to make the format the API urls properly when called upon
+class API_CALLS:
     def __init__(self, username=None, user_id=None, tweet_id=None):
         self.username = username
         self.user_id = user_id
@@ -15,5 +20,7 @@ class API_Calls:
         return 'https://api.twitter.com/2/users/by/username/' + self.username
     def get_tweets(self):
         return f"https://api.twitter.com/2/users/" + self.user_id + "/tweets?tweet.fields=created_at&max_results=100"
-    def get_tweets_info(self):
+    def get_tweets_txt_likes_views(self):
         return "https://api.twitter.com/2/tweets?ids=" + self.tweet_id + "&tweet.fields=public_metrics&expansions=attachments.media_keys&media.fields=public_metrics"
+    def get_tweets_create_date(self):
+        return "https://api.twitter.com/2/tweets?ids=" + self.tweet_id + "&tweet.fields=attachments,author_id,context_annotations,created_at,entities,geo,id,in_reply_to_user_id,lang,possibly_sensitive,public_metrics,referenced_tweets,source,text,withheld&expansions=referenced_tweets.id"
