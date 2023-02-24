df7d67ff9bbe8898c46a03883eca698f8adade69
BrycenAsay <brycen1177@gmail.com>
2023-02-23 20:03:57 -0700

    rewrote main file so it can access dates and tweet info in the same API call to allow more information per 15 minute session

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ logging.basicConfig(level=logging.DEBUG)
 
 if __name__ == '__main__':
    # define starting variables
-    USERNAME = _USERNAME
-    USER_ID = get_user_id()
-    sample_info = get_tweet_info(get_tweets())
-    engine = create_engine(f'mysql+pymysql://{USER}:{PASSWORD}@{HOST}/{DATABASE}')
-    sample_info.to_sql('twitter_data', engine)
+   USERNAME = _USERNAME
+   USER_ID = get_user_id()
+   TWEETS_AND_DATES = get_tweets()
+   print(TWEETS_AND_DATES)
+   sample_info = get_tweet_info(TWEETS_AND_DATES[0], TWEETS_AND_DATES[1])
+   engine = create_engine(f'mysql+pymysql://{USER}:{PASSWORD}@{HOST}/{DATABASE}')
+   sample_info.to_sql(f'twitter_data for user {USERNAME}', engine)
