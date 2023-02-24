282fbf18dba238907f708386f5174798c41c8319
BrycenAsay <brycen1177@gmail.com>
2023-02-23 18:20:02 -0700

    now imports username variable from config file

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from sqlalchemy import create_engine
-from config import HOST, USER, PASSWORD, DATABASE
+from config import HOST, USER, PASSWORD, DATABASE, _USERNAME
 import logging
 from API_Funcs import get_tweet_info, get_tweets, get_user_id
 logging.basicConfig(level=logging.DEBUG)
 
 if __name__ == '__main__':
    # define starting variables
-    USERNAME = 'RobTopGames'
+    USERNAME = _USERNAME
     USER_ID = get_user_id()
     sample_info = get_tweet_info(get_tweets())
     engine = create_engine(f'mysql+pymysql://{USER}:{PASSWORD}@{HOST}/{DATABASE}')
