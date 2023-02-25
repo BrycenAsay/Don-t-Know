7a005fe96e53e298cceb31b248a853276c9d5f65
BrycenAsay <brycen1177@gmail.com>
2023-02-25 00:42:10 -0700

    Added second function which takes OAuth generated tokens (as well as verifier) and retrives user specific oauth data for authorization to retrieve private metrics

--- OAuth.py
+++ OAuth.py
@@ -1,8 +1,9 @@
-from API_Funcs import get_OAuth_Tokens
+from API_Funcs import get_OAuth_Tokens, Access_Token
 
 def main():
     OAuth_Tokens = get_OAuth_Tokens()
-    print(OAuth_Tokens)
+    User_OAuth_Tokens = Access_Token(OAuth_Tokens)
+    print(User_OAuth_Tokens)
 
 if __name__ == '__main__':
     main()
