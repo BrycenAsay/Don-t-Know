ba301a482eb3bdb6c141238699c2a3486446f0fe
BrycenAsay <brycen1177@gmail.com>
2023-02-25 00:45:06 -0700

    Added access_OAuth_Tokens and OAUTH_ACCESS_TOKENS_HEADER to store a properly formated API URL that access OAuth tokens for a specific user

--- API_Calls.py
+++ API_Calls.py
@@ -1,13 +1,16 @@
 # creating a dictionary to store all twitter APIs and respective headers
 from config import BEARER_TOKEN as bear_token, API_KEY
 
-# headers for all API Calls
+# headers for all public API Calls
 HEADERS = {
     'Authorization': 'Bearer ' + bear_token,
     'Cookie': 'guest_id=v1%3A167354671801418520; guest_id_ads=v1%3A167354671801418520; guest_id_marketing=v1%3A167354671801418520; personalization_id="v1_Yg76iBwLIOAcAZZ145rAAA=="'
     }
-OAUTH_HEADERS = {
+OAUTH_GET_TOKENS_HEADERS = {
     'Cookie': '_twitter_sess=BAh7CSIKZmxhc2hJQzonQWN0aW9uQ29udHJvbGxlcjo6Rmxhc2g6OkZsYXNo%250ASGFzaHsABjoKQHVzZWR7ADoPY3JlYXRlZF9hdGwrCLmYfYSGAToMY3NyZl9p%250AZCIlNGMyZWMwN2Y0YzUwOTgwYzRjNWY5YWQ5MGMwOGNjMmY6B2lkIiU2YzM5%250ANjgzYmI5MTJiNDUwM2RlYmEwMDkyMjAxYThlNA%253D%253D--1cf2c36413e28fe1726e2ff12287c59076045c07; guest_id=v1%3A167354671801418520; guest_id_ads=v1%3A167354671801418520; guest_id_marketing=v1%3A167354671801418520; personalization_id="v1_Yg76iBwLIOAcAZZ145rAAA=="'
+}
+OAUTH_ACCESS_TOKENS_HEADERS = {
+  'Cookie': '_twitter_sess=BAh7CSIKZmxhc2hJQzonQWN0aW9uQ29udHJvbGxlcjo6Rmxhc2g6OkZsYXNo%250ASGFzaHsABjoKQHVzZWR7ADoPY3JlYXRlZF9hdGwrCLmYfYSGAToMY3NyZl9p%250AZCIlNGMyZWMwN2Y0YzUwOTgwYzRjNWY5YWQ5MGMwOGNjMmY6B2lkIiU2YzM5%250ANjgzYmI5MTJiNDUwM2RlYmEwMDkyMjAxYThlNA%253D%253D--1cf2c36413e28fe1726e2ff12287c59076045c07; guest_id=v1%3A167354671801418520; guest_id_ads=v1%3A167354671801418520; guest_id_marketing=v1%3A167354671801418520; personalization_id="v1_Yg76iBwLIOAcAZZ145rAAA=="'
 }
 
 # payload for all API Calls
@@ -15,11 +18,13 @@ PAYLOAD = {}
 
 # create a class called API_Calls which stores all urls and proper peramaters to make the format the API urls properly when called upon
 class API_CALLS:
-    def __init__(self, username=None, user_id=None, tweet_id=None, pag_token=''):
+    def __init__(self, username=None, user_id=None, tweet_id=None, pag_token='', oauth_token='', oauth_verifier=''):
         self.username = username
         self.user_id = user_id
         self.tweet_id = tweet_id
         self.pag_token = pag_token
+        self.oauth_token = oauth_token
+        self.oauth_verifier = oauth_verifier
     def get_user_id(self):
         return 'https://api.twitter.com/2/users/by/username/' + self.username
     def get_tweets(self):
@@ -28,3 +33,5 @@ class API_CALLS:
         return "https://api.twitter.com/2/tweets?ids=" + self.tweet_id + "&tweet.fields=public_metrics&expansions=attachments.media_keys&media.fields=public_metrics"
     def get_OAuth_Tokens(self):
         return f'https://api.twitter.com/oauth/request_token?oauth_consumer_key={API_KEY}&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1677300609&oauth_nonce=7QHNopWh1zp&oauth_version=1.0&oauth_signature=b2FjalXERzHbGVwzWnwIPN2Jf%2Fg%3D"'
+    def access_OAuth_Tokens(self):
+        return f'https://api.twitter.com/oauth/access_token?oauth_token={self.oauth_token}&oauth_verifier={self.oauth_verifier}&oauth_consumer_key={API_KEY}'
