0eea2be01b60bcdac0225cd7c0f21376f5b8353a
BrycenAsay <brycen1177@gmail.com>
2023-02-24 22:57:10 -0700

    Added get_OAuth_Tokens function which automatically retrives OAuth tokens and generates a URL through which the user can authenticate

--- API_Funcs.py
+++ API_Funcs.py
@@ -3,7 +3,7 @@ import requests
 import pandas as pd
 import time
 from IPython.display import display
-from API_Calls import API_CALLS, HEADERS, PAYLOAD
+from API_Calls import API_CALLS, OAUTH_HEADERS, HEADERS, PAYLOAD
 from config import _USERNAME
 
 # defines error handling for 429 api-limit-error, can handle other errors too if you want it to though
@@ -15,6 +15,16 @@ def error_handling(_data, _url, _headers):
             return _response.json()
     else:
         return _data
+
+# retrives OAuth Tokens
+def get_OAuth_Tokens(headers=OAUTH_HEADERS, payload=PAYLOAD, url = API_CALLS().get_OAuth_Tokens()):
+    O_Auth_Tokens = {}
+    response = requests.request("POST", url, headers=headers, data=payload)
+    data = response.text
+    O_Auth_Tokens['OAuth_Token'] = data[12:39]
+    O_Auth_Tokens['OAuth_Secret'] = data[59:91]
+    print('The user must visit THIS URL for Authorization purposes: ' + 'https://api.twitter.com/oauth/authorize?oauth_token=' + O_Auth_Tokens['OAuth_Token'])
+    return O_Auth_Tokens
 
 # retrives the user_id given a username
 def get_user_id(headers=HEADERS, payload=PAYLOAD, url = API_CALLS(username=_USERNAME).get_user_id()):
