3b7aa9704a17982aaac24be85da777cc5c912100
BrycenAsay <brycen1177@gmail.com>
2023-02-25 00:42:49 -0700

    Forgot to save my bad :')

--- OAuth.py
+++ OAuth.py
@@ -3,7 +3,6 @@ from API_Funcs import get_OAuth_Tokens, Access_Token
 def main():
     OAuth_Tokens = get_OAuth_Tokens()
     User_OAuth_Tokens = Access_Token(OAuth_Tokens)
-    print(User_OAuth_Tokens)
 
 if __name__ == '__main__':
     main()
