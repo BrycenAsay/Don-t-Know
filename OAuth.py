fe7e122343a839d80cd747219f2a5a1a73890d03
BrycenAsay <brycen1177@gmail.com>
2023-02-26 23:39:31 -0700

    added another main that basically just reads User OAuth information from the SQL table

--- OAuth.py
+++ OAuth.py
@@ -1,4 +1,6 @@
 from API_Funcs import get_OAuth_Tokens, Access_Token
+from sqlalchemy import create_engine
+import pandas as pd
 from config import HOST, USER, PASSWORD, DATABASE, _USERNAME
 import mysql.connector
 import simple_sql
@@ -15,6 +17,13 @@ def main():
         DATA.append(User_OAuth_Tokens[key])
     print([simple_sql.create_row('twitter_info.User_OAuth_Info', COLUMNS, DATA)])
 
+def another_main():
+    engine = create_engine(f'mysql+pymysql://{USER}:{PASSWORD}@{HOST}/{DATABASE}')
+    User_Auth = pd.DataFrame()
+    with engine.connect() as conn:
+        User_Auth = (pd.read_sql_table('User_OAuth_Info', conn)).to_dict()
+        print(User_Auth)
+
 """
     conn = mysql.connector.connect(user=f'{USER}', password=f'{PASSWORD}', host=f'{HOST}', database=f'{DATABASE}')
     with conn.cursor() as cursor:
@@ -30,4 +39,4 @@ def main():
 """
         
 if __name__ == '__main__':
-    main()
+    another_main()
