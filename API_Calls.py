3d59a498930b37197bb569fd4b372d3366ef1433
BrycenAsay <brycen1177@gmail.com>
2023-02-27 00:37:45 -0700

    added get_tweets_time_specific API that is the same as the get_tweets API but it specifically accesses tweets from the past 30 days

--- API_Calls.py
+++ API_Calls.py
@@ -1,5 +1,6 @@
 # creating a dictionary to store all twitter APIs and respective headers
 from config import BEARER_TOKEN as bear_token, API_KEY
+from datetime import datetime, date, timedelta
 
 # headers for all public API Calls
 HEADERS = {
@@ -25,13 +26,19 @@ class API_CALLS:
         self.pag_token = pag_token
         self.oauth_token = oauth_token
         self.oauth_verifier = oauth_verifier
+        current_time = datetime.now()
+        self.end_time = f'{str(current_time)[0:10]}T{str(current_time)[11:20]}000Z'
+        thirty_days_back = str(date.today()-timedelta(days=30))
+        self.start_time = f'{thirty_days_back}T{str(current_time)[11:20]}000Z'
     def get_user_id(self):
         return 'https://api.twitter.com/2/users/by/username/' + self.username
     def get_tweets(self):
         return f"https://api.twitter.com/2/users/" + self.user_id + "/tweets?tweet.fields=created_at&max_results=5" + self.pag_token
+    def get_tweets_time_specific(self):
+        return f"https://api.twitter.com/2/users/{self.user_id}/tweets?tweet.fields=created_at&max_results=100&start_time={self.start_time}&end_time={self.end_time}"
     def get_tweets_txt_likes_views(self):
         return "https://api.twitter.com/2/tweets?ids=" + self.tweet_id + "&tweet.fields=public_metrics&expansions=attachments.media_keys&media.fields=public_metrics"
     def get_OAuth_Tokens(self):
-        return f'https://api.twitter.com/oauth/request_token?oauth_consumer_key={API_KEY}&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1677300609&oauth_nonce=7QHNopWh1zp&oauth_version=1.0&oauth_signature=b2FjalXERzHbGVwzWnwIPN2Jf%2Fg%3D"'
+        return f'https://api.twitter.com/oauth/request_token?oauth_consumer_key={API_KEY}&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1677427442&oauth_nonce=wVsQ2P8nTbb&oauth_version=1.0&oauth_signature=3BSY1kX9DuLKypRMfc4%2Fn3nHgJE%3D'
     def access_OAuth_Tokens(self):
         return f'https://api.twitter.com/oauth/access_token?oauth_token={self.oauth_token}&oauth_verifier={self.oauth_verifier}&oauth_consumer_key={API_KEY}'
