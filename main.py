e21e8a335f3f73fce52372ced06471d68f16c2ff
BrycenAsay <brycen1177@gmail.com>
2023-03-06 23:56:14 -0700

    merged all of the 'main_one, main_two, etc.' to all execute at the same time, then sorted them into different chunks depending on what information they write to the database

--- main.py
+++ main.py
@@ -1,29 +1,34 @@
 from sqlalchemy import create_engine
 from config import HOST, USER, PASSWORD, DATABASE, _USERNAME
 import logging
-from API_Funcs import get_tweet_info, get_tweets, get_user_id, get_following, get_followers
+from API_Funcs import get_tweet_info, get_tweets, get_following, get_followers, get_private_tweet_info, token_retrieval
 logging.basicConfig(level=logging.DEBUG)
 
-def main_one():
-   # define starting variables
+def main():
+   # retrive the user we are getting all the twitter info for
    USERNAME = _USERNAME
-   USER_ID = get_user_id()
-   TWEETS_AND_DATES = get_tweets()
-   print(TWEETS_AND_DATES)
-   sample_info = get_tweet_info(TWEETS_AND_DATES[0], TWEETS_AND_DATES[1])
    engine = create_engine(f'mysql+pymysql://{USER}:{PASSWORD}@{HOST}/{DATABASE}')
+
+   # process the public metrics for all given tweets and write to a SQL table
+   TWEETS_AND_DATES = get_tweets()
+   sample_info = get_tweet_info(TWEETS_AND_DATES[0], TWEETS_AND_DATES[1])
    sample_info.to_sql(f'twitter_data for user {USERNAME}', engine)
 
-def main_two():
-   USERNAME = _USERNAME
-   USER_ID = get_user_id
+   # process the followers/following for the user and write to 2 seperate tables
    following = get_following()
    ing_and_ers = get_followers(following)
    following = ing_and_ers[0]
    followers = ing_and_ers[1]
-   engine = create_engine(f'mysql+pymysql://{USER}:{PASSWORD}@{HOST}/{DATABASE}')
    followers.to_sql(f'users following {USERNAME}', engine)
    following.to_sql(f'users {USERNAME} is following', engine)
 
+   # process the private metrics for tweets within the past 30 days and write to a SQL table
+   tweets = get_tweets(time_specific=True)
+   USER_TOKENS = token_retrieval()
+   USER_TOKEN = USER_TOKENS[0]
+   TOKEN_SECRET = USER_TOKENS[1]
+   private_metrics = get_private_tweet_info(tweets[0], tweets[1], USER_TOKEN, TOKEN_SECRET)
+   private_metrics.to_sql(f'private twitter_data for user {USERNAME}', engine)
+
 if __name__ == '__main__':
-   main_two()
+   main()
