f9c120c4554f81c931cc45a3dbdff2985309f522
BrycenAsay <brycen1177@gmail.com>
2023-03-06 23:55:14 -0700

    cleaned up all of the junk, esentially just simplified the file to it's sole purpose: retriving and storing user OAuth data into a table

--- OAuth.py
+++ OAuth.py
@@ -1,42 +1,18 @@
 from API_Funcs import get_OAuth_Tokens, Access_Token
-from sqlalchemy import create_engine
-import pandas as pd
-from config import HOST, USER, PASSWORD, DATABASE, _USERNAME
-import mysql.connector
 import simple_sql
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
 def main():
+    # automation of the 3-legged OAuth process for getting more permanent tokens for a user (at least as much as I could automate the process)
     COLUMNS = []
     DATA = []
     OAuth_Tokens = get_OAuth_Tokens()
     User_OAuth_Tokens = Access_Token(OAuth_Tokens)
     for key in User_OAuth_Tokens:
         COLUMNS.append(key)
-        DATA.append(User_OAuth_Tokens[key])
-    print([simple_sql.create_row('twitter_info.User_OAuth_Info', COLUMNS, DATA)])
-
-def another_main():
-    engine = create_engine(f'mysql+pymysql://{USER}:{PASSWORD}@{HOST}/{DATABASE}')
-    User_Auth = pd.DataFrame()
-    with engine.connect() as conn:
-        User_Auth = (pd.read_sql_table('User_OAuth_Info', conn)).to_dict()
-        print(User_Auth)
-
-"""
-    conn = mysql.connector.connect(user=f'{USER}', password=f'{PASSWORD}', host=f'{HOST}', database=f'{DATABASE}')
-    with conn.cursor() as cursor:
-        sql = simple_sql.create_row('twitter_info.User_OAuth_Info', COLUMNS, DATA)
-        try:
-            cursor.execute(sql)
-            conn.commit()
-        except:
-            logging.exception('Did not work')
-            conn.rollback()
-        
-        conn.close()
-"""
+        DATA.append(str(User_OAuth_Tokens[key]))
+    print("Use this SQL statement to insert the data into the User_OAuth_Info table: " + simple_sql.create_row('twitter_info.User_OAuth_Info', COLUMNS, DATA))
         
 if __name__ == '__main__':
-    another_main()
+    main()
